838d5bdfa534930bbb31f42d027e9faffed646d3
Nikitand <nikitos_by_brest@mail.ru>
2021-07-14 13:54:46 +0300

    Error

--- bikeCard/force-app/main/default/lwc/contactList/contactList.js
+++ bikeCard/force-app/main/default/lwc/contactList/contactList.js
@@ -1,29 +1,30 @@
 import { LightningElement, wire } from 'lwc';
-import FIRST_NAME_FIELD from '@salesforce/schema/Contact.FirstName';
-import LAST_NAME_FIELD from '@salesforce/schema/Contact.LastName';
-import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import CONTACT_FIRSTNAME from '@salesforce/schema/Contact.FirstName';
+import CONTACT_LASTNAME from '@salesforce/schema/Contact.LastName';
+import CONTACT_EMAIL from '@salesforce/schema/Contact.Email';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
 import { reduceErrors } from 'c/ldsUtils';
 const COLUMNS = [
-    { label: 'Contact First Name', fieldName: FIRST_NAME_FIELD.fieldApiName, type: 'text' },
-    { label: 'Contact Last Name', fieldName: LAST_NAME_FIELD.fieldApiName, type: 'currency' },
-    { label: 'Email', fieldName: EMAIL_FIELD.fieldApiName, type: 'text' }
+{ label: 'First Name',fieldName: CONTACT_FIRSTNAME.fieldApiName, type: 'text' },
+{ label: 'Last Name', fieldName: CONTACT_LASTNAME.fieldApiName, type: 'text' },
+{ label: 'Email', fieldName: CONTACT_EMAIL.fieldApiName, type: 'email' }
 ];
 export default class ContactList extends LightningElement {
-    columns = COLUMNS;
-    @wire(getContacts)
-    error;
-    wiredContacts({ error, data }) {
-    if (data) {
-    this.contacts = data;
-    this.error = undefined;
-    } else if (error) {
-    this.error = error;
-    this.contacts = undefined;
-    }
-    }
-    get errors() {
-    return (this.accounts.error) ?
-    reduceErrors(this.accounts.error) : [];
-    }
-    }
+columns = COLUMNS;
+@wire(getContacts)
+contacts;
+error;
+wiredContacts({ error, data }) {
+if (data) {
+this.contacts = data;
+this.error = undefined;
+} else if (error) {
+this.error = error;
+this.contacts = undefined;
+}
+}
+get errors() {
+return (this.accounts.error) ?
+reduceErrors(this.accounts.error) : [];
+}
+}
